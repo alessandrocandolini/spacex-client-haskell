4487e60fbf366521079edb589a150d2245a55335
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-12-20 10:12:54 +0000

    migrate to sydtest

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -1,14 +1,13 @@
 module AppSpec where
 
 import App
-import Test.Hspec
-import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
+import Test.Syd
 
 spec :: Spec
 spec = describe "Simple test" $ do
   it "example-based unit test" $
-    1 `shouldBe` 1
+    1 == 1
 
   prop "property-based unit test" $
     \l -> reverse (reverse l) == (l :: [Int])
