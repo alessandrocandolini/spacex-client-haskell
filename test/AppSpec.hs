a4b90e0b477558281b8e0bd4f133ea9f5a9f4264
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-01-08 17:27:59 +0000

    Fixed test

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -7,7 +7,7 @@ import Test.Syd
 spec :: Spec
 spec = describe "Simple test" $ do
   it "example-based unit test" $
-    1 `shouldBe` 2
+    2 `shouldBe` 2
 
   prop "property-based unit test" $
     \l -> reverse (reverse l) == (l :: [Int])
